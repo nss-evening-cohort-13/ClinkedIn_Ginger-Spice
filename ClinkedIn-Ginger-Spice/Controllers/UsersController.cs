0727cc4bcbb3e1bf7c0b5f2a345388126da0865c
Liz Barnes <embarnes95@gmail.com>
2021-02-22 18:57:40 -0800

    create user model, repository, and controllers to get user data

---
+++ ClinkedIn-Ginger-Spice/Controllers/UsersController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ClinkedIn_Ginger_Spice.DataAccess;
+
+namespace ClinkedIn_Ginger_Spice.Controllers
+{
+    
+    [Route("api/Users")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        UserRepository _repo;
+
+        public UsersController()
+        {
+            _repo = new UserRepository();
+        }
+
+        [HttpGet]
+        public IActionResult GetAllUsers()
+        {
+            return Ok(_repo.GetAll());
+        }
+    }
+}
---
+++ ClinkedIn-Ginger-Spice/DataAccess/UserRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ClinkedIn_Ginger_Spice.Models;
+
+namespace ClinkedIn_Ginger_Spice.DataAccess
+{
+    public class UserRepository
+    {
+        static List<User> _users = new List<User>
+        {
+            new User { Id = 1, Email = "abc@gmail.com", Password = "abc", FirstName = "Alpha", LastName = "Bet", Gender = Gender.Nonbinary, Age = 26 },
+            new User { Id = 2, Email = "def@gmail.com", Password = "def", FirstName = "Beta", LastName = "Fish", Gender = Gender.Nonbinary, Age = 26 },
+            new User { Id = 3, Email = "ghi@gmail.com", Password = "ghi", FirstName = "Charlie", LastName = "Horse", Gender = Gender.Nonbinary, Age = 26 },
+            new User { Id = 4, Email = "jkl@gmail.com", Password = "jkl", FirstName = "Delta", LastName = "Airlines", Gender = Gender.Nonbinary, Age = 26 }
+        };
+
+        public List<User> GetAll()
+        {
+            return _users;
+        }
+    }
+}
